9dd01f1e210a66cf47ef92b9f8f57056461e6613
Henry Nwadiogor <nwadiogor@rsn.local>
2020-08-16 12:06:26 +0200

    Added email & password validation to AuthController and bind it to  login dot vue file

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,8 +28,14 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login()
+    public function login(Request $request)
     {
+        // setting validation for email and password
+        $validateData = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
         $credentials = request(['email', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -12,10 +12,14 @@
                                     </div>
                                     <form class="user" @submit.prevent="login">
                                         <div class="form-group">
-                                            <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address" v-model="form.email">
+                                            <input type="email" class="form-control" id="exampleInputEmail" 
+                                            aria-describedby="emailHelp" placeholder="Enter Email Address" v-model="form.email">
+                                            <small class="text-danger" v-if="errors.email"> {{ errors.email[0] }} </small>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password" v-model="form.password">
+                                            <input type="password" class="form-control" id="exampleInputPassword"
+                                             placeholder="Password" v-model="form.password">
+                                             <small class="text-danger" v-if="errors.password"> {{ errors.password[0] }} </small>
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
@@ -60,9 +64,9 @@
                 form: {
                     email: null,
                     password: null
-                }
+                },
+                errors:{}
             }
-
         },
 
         methods: {
